619a5aad0280bad5cc884d4055605a26bb3d0025
Giovanni Henrique <giovannihpf@gmail.com>
2025-04-02 00:41:54 -0300

    Criando componentes para Timer e Button

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { PomodoroTimer } from './components/pomodoro-timer';
 function App() {
   return (
     <div className="App">
-      <PomodoroTimer defaultPomodoroTime={1500} />
+      <PomodoroTimer defaultPomodoroTime={60} />
       Olá mundo
     </div>
   );
