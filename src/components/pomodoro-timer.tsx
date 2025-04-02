71d78b9af4ba7c04518b68062fef83ecadbc9bc7
Giovanni Henrique <giovannihpf@gmail.com>
2025-04-02 20:49:48 -0300

    Criando funcionalidades dos buttons Work & Play/Pause

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInterval } from '../hooks/use-interval';
 import { Button } from './button';
 import { Timer } from './timer';
@@ -10,20 +10,35 @@ interface Props {
 }
 export function PomodoroTimer(props: Props) {
   const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
+  const [timeCouting, setTimeCouting] = React.useState(false);
+  const [working, setWorking] = React.useState(false);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeCouting ? 1000 : null,
+  );
 
+  const configureWork = () => {
+    setTimeCouting(true);
+    setWorking(true);
+  };
   return (
     <div className="pomodoro">
       <h2> You are: working</h2>
       <Timer mainTime={mainTime} />
 
       <div className="controls">
+        <Button text="Work" onClick={() => console.log(configureWork())} />
         <Button text="teste" onClick={() => console.log(1)} />
-        <Button text="teste" onClick={() => console.log(1)} />
-        <Button text="teste" onClick={() => console.log(1)} />
+        <Button
+          text={timeCouting ? 'Pause' : 'Play'}
+          onClick={() => setTimeCouting(!timeCouting)}
+        />
       </div>
       <div className="details">
         <p>Testando: Lorem ipsum dolor sit amet!</p>
